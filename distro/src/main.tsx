9ff6839b77c904629b12ab8a6617668328c0ab2c
Lingeswaran Subramaniyam <lingeswaran.s@thoughtworks.com>
2026-01-08 15:44:18 +0530

    Test CA

--- distro/src/main.tsx
+++ distro/src/main.tsx
@@ -7,6 +7,7 @@ import App from './app/app';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+//Hi
 root.render(
   <StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL ?? '/'}>
