5b914290bc1bb8de5fa8ffd57e55d27d069a788f
sahupadma <padma.sahu@thoughtworks.com>
2026-02-26 14:42:03 +0530

    BAH-4413 | Fix. failing clinical tests

--- setupTests.ts
+++ setupTests.ts
@@ -4,3 +4,53 @@ import { TextEncoder, TextDecoder } from 'util';
 global.TextEncoder = TextEncoder;
 // @ts-expect-error - Ignoring type issues with Node.js util TextDecoder
 global.TextDecoder = TextDecoder;
+
+// Mock ResizeObserver
+global.ResizeObserver = class ResizeObserver {
+  constructor(_callback: ResizeObserverCallback) {
+    void _callback;
+  }
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
+
+// Mock DOMRect
+global.DOMRect = class DOMRect {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  top: number;
+  right: number;
+  bottom: number;
+  left: number;
+
+  constructor(x = 0, y = 0, width = 0, height = 0) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.top = y;
+    this.right = x + width;
+    this.bottom = y + height;
+    this.left = x;
+  }
+
+  toJSON() {
+    return {
+      x: this.x,
+      y: this.y,
+      width: this.width,
+      height: this.height,
+      top: this.top,
+      right: this.right,
+      bottom: this.bottom,
+      left: this.left,
+    };
+  }
+
+  static fromRect(other?: DOMRectInit): DOMRect {
+    return new DOMRect(other?.x || 0, other?.y || 0, other?.width || 0, other?.height || 0);
+  }
+};
