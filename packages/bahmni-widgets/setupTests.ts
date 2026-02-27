c2f755fb4c7a20239f3e49e9c88fa14f6f571d4e
sahupadma <padma.sahu@thoughtworks.com>
2026-02-27 10:46:03 +0530

    BAH-4390 | Revert. failing tests

--- packages/bahmni-widgets/setupTests.ts
+++ packages/bahmni-widgets/setupTests.ts
@@ -6,13 +6,7 @@ import './setupTests.i18n';
 // Initialize FontAwesome icons for tests
 initFontAwesome();
 
+// @ts-expect-error - Ignoring type issues with Node.js util TextEncoder
 global.TextEncoder = TextEncoder;
 // @ts-expect-error - Ignoring type issues with Node.js util TextDecoder
 global.TextDecoder = TextDecoder;
-
-// Mock ResizeObserver for components that use it
-global.ResizeObserver = jest.fn().mockImplementation(() => ({
-  observe: jest.fn(),
-  unobserve: jest.fn(),
-  disconnect: jest.fn(),
-}));
