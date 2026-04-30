6dc671a427fc7921081978ceff520ce70ed652a8
arshiyaTW2021 <arshiya.shehzad@thoughtworks.com>
2026-04-30 12:47:54 +0530

    BAH-4519 | Fix. Scope LocationProvider and QueryClientProvider to IndexPage instead of all routes

--- distro/src/app/IndexPage.tsx
+++ distro/src/app/IndexPage.tsx
@@ -4,22 +4,32 @@ import {
   NotificationServiceComponent,
   UserPrivilegeProvider,
 } from '@bahmni/widgets';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import { HomePageGrid } from './components/HomePageGrid';
 import { HomePageHeader } from './components/HomePageHeader';
+import { LocationProvider } from './context';
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: false, refetchOnWindowFocus: false } },
+});
 
 export const IndexPage: React.FC = () => {
   return (
-    <ActivePractitionerProvider>
-      <UserPrivilegeProvider>
-        <NotificationProvider>
-          <NotificationServiceComponent />
-          <HomePageHeader />
-          <main>
-            <HomePageGrid />
-          </main>
-        </NotificationProvider>
-      </UserPrivilegeProvider>
-    </ActivePractitionerProvider>
+    <LocationProvider>
+      <QueryClientProvider client={queryClient}>
+        <ActivePractitionerProvider>
+          <UserPrivilegeProvider>
+            <NotificationProvider>
+              <NotificationServiceComponent />
+              <HomePageHeader />
+              <main>
+                <HomePageGrid />
+              </main>
+            </NotificationProvider>
+          </UserPrivilegeProvider>
+        </ActivePractitionerProvider>
+      </QueryClientProvider>
+    </LocationProvider>
   );
 };
