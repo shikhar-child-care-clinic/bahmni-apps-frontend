773e17f8464f720078de5d78ee12ec425fc1a047
arshiyaTW2021 <arshiya.shehzad@thoughtworks.com>
2026-04-30 13:17:54 +0530

    BAH-4519. Add LOGIN_PATH and CHANGE_PASSWORD_PATH in app.ts

--- distro/src/constants/app.ts
+++ distro/src/constants/app.ts
@@ -1,3 +1,5 @@
 export const PUBLIC_PATH = process.env.PUBLIC_PATH ?? '/';
 export const CACHE_MAX_AGE = 7 * 24 * 60 * 60;
 export const HOME_NAMESPACE = 'home';
+export const LOGIN_PATH = '/bahmni/home/index.html#/login';
+export const CHANGE_PASSWORD_PATH = '/bahmni/home/index.html#/changePassword';
