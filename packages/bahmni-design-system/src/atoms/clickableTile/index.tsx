3f71665944a48eb039912d6f2926cc842639a1e2
arshiyaTW2021 <arshiya.shehzad@thoughtworks.com>
2026-05-11 15:42:44 +0530

    BAH-4669. Remove redundant data-testid from clickableTile index

--- packages/bahmni-design-system/src/atoms/clickableTile/index.tsx
+++ packages/bahmni-design-system/src/atoms/clickableTile/index.tsx
@@ -6,17 +6,15 @@ import React from 'react';
 
 export type ClickableTileProps = CarbonClickableTileProps & {
   testId?: string;
-  'data-testid'?: string;
 };
 
 export const ClickableTile: React.FC<ClickableTileProps> = ({
   testId,
-  'data-testid': dataTestId,
   children,
   ...carbonProps
 }) => {
   return (
-    <CarbonClickableTile {...carbonProps} data-testid={testId ?? dataTestId}>
+    <CarbonClickableTile {...carbonProps} data-testid={testId}>
       {children}
     </CarbonClickableTile>
   );
